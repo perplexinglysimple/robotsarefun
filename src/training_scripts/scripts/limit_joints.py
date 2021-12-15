f53dfdb31894c9bf341a6a7f369a750e2c36a870
George Boyer <boyjorge@vt.edu>
2021-12-15 12:04:39 -0700

    OMG So much stuff ugh

--- src/training_scripts/scripts/limit_joints.py
+++ src/training_scripts/scripts/limit_joints.py
@@ -1,8 +1,8 @@
 import rospy
 import json
 import numpy as np
-from webots_ros.msgs import JointCommand
-
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64
 
 class LimitingJointClass:
     def __init__(self):
@@ -13,7 +13,7 @@ class LimitingJointClass:
     #This is the only function you want to call from the outside
     #You can call this to set new limits at any time
     def setupJointLimits(self, filename):
-        rospy.logdebug('loading new filename', filename)
+        rospy.logdebug('loading new filename {}'.format(filename))
         self.setup_new_limits(self.loadJson(filename))
 
     def loadJson(self, filename):
@@ -22,15 +22,16 @@ class LimitingJointClass:
         return data
 
     def setup_sub_pub_pairs(self, sub_name, pub_name):
-        if pub_name in self.publishers.keys():
-            self.publishers[pub_name] = rospy.Publisher(pub_name, JointCommand, queue_size=1)
-            self.subArr.append(rospy.Subscriber(sub_name, JointCommand, self.callback, (pub_name)))
+        if not pub_name in self.publishers.keys():
+            self.publishers[pub_name] = rospy.Publisher(pub_name, JointState, queue_size=1)
+            self.subArr.append(rospy.Subscriber(sub_name, JointState, self.callback, (pub_name)))
 
     def setup_new_limits(self, data):
-        rospy.logdebug('Setting up new limits')
-        rospy.logdebug(data)
+        rospy.loginfo('Setting up new limits')
+        rospy.loginfo(data)
         joint_names = data.keys()
         for i in joint_names:
+            i = data[i]
             sub_name = i['sub']
             pub_name = i['pub']
             joint_limit_angle_min = i['Amin']
@@ -47,18 +48,25 @@ class LimitingJointClass:
         jointDict = self.jointInfo[pub_name]
         #This can be changed to fit failure types. Just dont know what to do here tbh.
         failure_type = jointDict['FailureClass']
-        rospy.logdebug('Initial msg', pub_name, msg)
+        rospy.logdebug('Initial msg')
+        rospy.loginfo(pub_name)
+        rospy.loginfo(msg)
+        rospy.loginfo(Float64(msg.position[0]))
         if failure_type == 'angle' or failure_type == 'both':
-            msg.angle = np.clip(msg.angle, jointDict['Amin'], jointDict['Amax'])
-        if failure_type == 'angle' or failure_type == 'both':
-            msg.torque = np.clip(msg.angle, jointDict['Tmin'], jointDict['Tmax'])
-        rospy.logdebug('Limited msg', pub_name, msg)
+            msg.position = (float(np.clip(msg.position[0], jointDict['Amin'], jointDict['Amax'])),)
+        #if failure_type == 'torque' or failure_type == 'both':
+        #    msg.torque = np.clip(msg.angle, jointDict['Tmin'], jointDict['Tmax'])
+        rospy.logdebug('Limited msg')
+        rospy.logdebug(pub_name)
+        rospy.logdebug(msg)
         self.publishers[pub_name].publish(msg)
 
 
-if __name == "__main__":
-    rospy.info("initing the joint limiting class")
+if __name__ == "__main__":
+    rospy.init_node('robot_limiter', anonymous=True)
+    rospy.loginfo("initing the joint limiting class")
     test = LimitingJointClass()
     test.setupJointLimits('testJointLimits.json')
+    rospy.loginfo("done")
     #You can use a rosrate here or something to wait to set new limits.
     rospy.spin()
